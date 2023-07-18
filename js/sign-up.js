c1be14e5f95ca9e8c2470970e20403be0ac93604
Reza <i.am.reza.programmer@gmail.com>
2023-07-18 10:59:46 +0330

    Throwing back-end files out of the project and fixing bug response

--- js/sign-up.js
+++ js/sign-up.js
@@ -1,16 +1,27 @@
 const $ = document;
 const eyeIconList = $.querySelectorAll(".eye-icon");
 const inputList = $.querySelectorAll(".input");
-
+const formBtn = $.querySelector(".form-btn");
+const regexEmail = /[a-zA-Z0-9.-]+@[a-z-]+\.[a-z]{2,3}/;
+let userName = inputList[0];
+let email = inputList[1];
+let password = inputList[2];
+let password2 = inputList[3];
 // set event for icons
 eyeIconList.forEach((icon) => {
   icon.addEventListener("click", (event) => {
     if (event.target.className == "fas fa-eye eye-icon") {
       event.target.className = "fas fa-eye-slash eye-icon";
-      event.target.previousElementSibling.setAttribute("type", "text");
+      event.target.previousElementSibling.previousElementSibling.setAttribute(
+        "type",
+        "text"
+      );
     } else {
       event.target.className = "fas fa-eye eye-icon";
-      event.target.previousElementSibling.setAttribute("type", "password");
+      event.target.previousElementSibling.previousElementSibling.setAttribute(
+        "type",
+        "password"
+      );
     }
   });
 });
@@ -29,3 +40,52 @@ inputList.forEach((input) => {
     }
   });
 });
+
+// set event for btn
+formBtn.addEventListener("click", (event) => {
+  if (checkInputs()) {
+    const url = "http://localhost:8000/accounts/register/";
+    const header = {
+      "Content-Type": "application/json",
+    };
+    const data = {
+      username: userName.value,
+      email: email.value,
+      password: password.value,
+      password2: password2.value,
+    };
+
+    // fetch(url, {
+    //   method: "POST",
+    //   headers: header,
+    //   body: JSON.stringify(data),
+    // })
+    //   .then((res) => console.log(res))
+    //   .catch((err) => console.log(err));
+
+    axios
+      .post(url, data, {
+        "Content-Type": "application/json",
+      })
+      .then((res) => console.log(res.data))
+      .catch((err) => console.log(err));
+  } else {
+    console.log("error");
+  }
+});
+
+// check inputs value
+function checkInputs() {
+  if (
+    userName.value.length >= 3 &&
+    userName.value.length <= 12 &&
+    email.value.match(regexEmail) &&
+    password.value.length >= 8 &&
+    password.value.length <= 15 &&
+    +password.value == +password2.value
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
