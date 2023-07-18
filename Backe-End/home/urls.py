65a330cbc0a4c6aca9f8a2ca7ae1b93fd3b70e28
arshia <arshia@email.com>
2023-07-18 18:36:15 +0330

    AHa

--- Backe-End/home/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'home'
-urlpatterns = [
-    path('', views.HomeView.as_view(), name="home"),
-    path('explore', views.ExploreView.as_view(), name="explore"),
-
-]
